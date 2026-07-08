e1f3674ffe6f9a4441e62b3dc1c9cdaef0281641
Douglas Jasper <doug.jasper@trios.com>
2026-07-08 09:08:24 -0400

    Updated ThemeSelector2026App.swift

--- ThemeSelector2026/ThemeSelector2026App.swift
+++ ThemeSelector2026/ThemeSelector2026App.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct ThemeSelector2026App: App {
+    
+    @StateObject private var settings = Settings()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(settings)
+                .preferredColorScheme(colorScheme(for: settings.themeMode))
+        }
+    }
+    
+    private func colorScheme(for mode: ThemeMode) -> ColorScheme? {
+        switch mode {
+            case .system:
+                return nil
+            case .light:
+                return .light
+            case .dark:
+                return .dark
         }
     }
 }
